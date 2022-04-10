69000f428d3334c7c6be45b722f995a3e9d5c3c8
uyeonH <yooyeon0508@gmail.com>
2022-04-10 11:51:34 +0900

    조건에 따라 테스트 실행

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Test
+
+## Assertion
+
+- `assertEquals(expected, actual)` 실제 값이 기대한 값과 같은지 확인
+
+- `assertNotNull(actual)` 값이 null이 아닌지 확인
+
+- `assertTrue(boolean)` 다음 조건이 참(true)인지 확인
+
+- `assertAll(executables...)` 모든 확인 구문 확인
+
+- `assertThrows(expectedType, executable)` 예외 발생 확인
+
+- `assertTimeout(duration, executable)` 특정 시간 안에 실행이 완료되는지 확인
--- src/test/java/yooyeon/test/StudyTest.java
+++ src/test/java/yooyeon/test/StudyTest.java
@@ -1,13 +1,75 @@
 package yooyeon.test;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 //@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
+
+
+    @Test
+    @EnabledOnOs({OS.MAC, OS.WINDOWS})
+    @DisabledOnOs(OS.LINUX)
+    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_9, JRE.JAVA_10, JRE.JAVA_11})
+    void condition2() {
+        // OS 환경 제한
+        // JRE 버전 제한
+    }
+
+    @Test
+    //@EnabledIfEnvironmentVariable(named = "TEST_ENV_STRING", matches = "Test")
+    void condition() {
+        // 조건을 만족하는 경우 테스트를 진행하고 싶음
+
+        // 시스템 환경변수에 TEST_ENV_STRING: Test인 경우우
+        String test_env = System.getenv("TEST_ENV_STRING");
+
+        System.out.println("test_env = " + test_env);
+        assumeTrue("Test".equalsIgnoreCase(test_env));
+
+        assumingThat("Test".equalsIgnoreCase(test_env), () -> {
+            Study actual = new Study(10);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        });
+
+    }
+
+    @Test
+    void exception() {
+        // 특정 예외가 발생하는지 확인
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = exception.getMessage();
+        assertEquals("limit은 0보다 커야함", message);
+    }
+
+    @Test
+    void time_out() {
+
+        /*
+        assertTimeout(Duration.ofMillis(100),()->
+        {
+            new Study(10);
+            Thread.sleep(1000);
+        });
+        */
+
+        // 실행이 완료될 때까지 기다리지 않고 100ms를 넘으면 실패하게 하고싶다면
+        // 단, ThreadLocal을 사용하는 코드에서는 사용하지 말것
+        assertTimeoutPreemptively(Duration.ofMillis(100), () ->
+        {
+            new Study(10);
+            Thread.sleep(10);
+        });
+
+    }
 
     @Test
     @DisplayName("스터디 생성")
@@ -21,10 +83,6 @@ class StudyTest {
                 () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 인원은 0보다 커야한다")
         );
 
-        // 특정 예외가 발생하는지 확인
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
-        String message = exception.getMessage();
-        assertEquals("limit은 0보다 커야함", message);
     }
 
     @Test
